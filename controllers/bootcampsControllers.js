48d9fffa93e358a8b490bd3098709fb9f5505ff1
Noor Mohammad <noormohammad.net1503011@gmail.com>
2023-01-12 00:13:26 +0600

    geolocation controller

--- controllers/bootcampsControllers.js
+++ controllers/bootcampsControllers.js
@@ -1,6 +1,7 @@
 import asyncHandler from 'express-async-handler'
 import Bootcamp from '../models/bootCampModel.js'
 import ErrorResponse from '../utils.js/errorResponse.js'
+import geocoder from '../utils.js/geocoder.js'
 // @desc      Get all bootcamps
 // @route     GET /api/v1/bootcamps
 // @access    Public
@@ -68,10 +69,38 @@ const deleteBootcamp = asyncHandler(async (req, res, next) => {
   res.status(200).json({ success: true, data: {} })
 })
 
+// @desc      Get bootcamps within a radius
+// @route     GET /api/v1/bootcamps/radius/:zipcode/:distance
+// @access    Private
+const getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+  const { zipcode, distance } = req.params;
+
+  // Get lat/lng from geocoder
+  const loc = await geocoder.geocode(zipcode);
+  const lat = loc[0].latitude;
+  const lng = loc[0].longitude;
+
+  // Calc radius using radians
+  // Divide dist by radius of Earth
+  // Earth Radius = 3,963 mi / 6,378 km
+  const radius = distance / 3963;
+
+  const bootcamps = await Bootcamp.find({
+    location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } }
+  });
+
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    data: bootcamps
+  });
+});
+
 export {
   getBootcamps,
   getBootcamp,
   deleteBootcamp,
   createBootcamp,
   updateBootcamp,
+  getBootcampsInRadius,
 }
--- routes/bootcampsRoutes.js
+++ routes/bootcampsRoutes.js
@@ -4,11 +4,13 @@ import {
   deleteBootcamp,
   getBootcamp,
   getBootcamps,
+  getBootcampsInRadius,
   updateBootcamp,
 } from '../controllers/bootcampsControllers.js'
 
 const router = express.Router()
 
+router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius)
 router.route('/').get(getBootcamps).post(createBootcamp)
 router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp)
 
